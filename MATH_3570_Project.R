6fd93b85858bf972ffa0def7f9118a23bce27c1f
Yatagarasu50469 <davidpablo50469@yahoo.com>
2018-03-07 14:48:02 -0600

    Formatting and Structure
    Added some formatting and added notes for TODOs

--- MATH_3570_Project.R
+++ MATH_3570_Project.R
@@ -5,6 +5,10 @@
 #  Date Last Modified:    6 March 2018
 #  Purpose:               Clean and analyze data found for weather, traffic and crime within Chicago for years 2013-2015
 
+#
+# SETUP
+#
+
 #Library imports
 library(tidyverse)
 library(reshape2)
@@ -12,10 +16,16 @@ library(reshape2)
 #Set working directory to location where data is stored
 setwd("~/Desktop/MATH 3570 Project/data")
 
-#Import initial datasets
+#
+#IMPORT DATASETS
+#
 weatherData <- read.csv("Chicago_Midway_Airport_Weather Station.csv")
 trafficData <- read.csv("Chicago_Traffic_Tracker_-_Historical_Congestion_Estimates_by_Segment.csv")
 crimeData <- read.csv("Crimes_-_2001_to_present.csv")
+
+#
+#FILTER BY DATE
+#
 
 #Ignoring potential discrepencies caused by time zones, as primary interest is on daily information
 #Filter out information to just that obtained between Jan 1 2013 through Dec 30 2014 (Overlapping years for all three datasets)
@@ -34,7 +44,6 @@ newCrime$Date <- as.Date(ISOdate(newCrime$Year, newCrime$Month, newCrime$Day))
 newCrime$Year = NULL
 newCrime$Month = NULL
 newCrime$Day = NULL
-
 #Filter out information to just that obtained between Jan 1 2013 through Dec 30 2014 (Overlapping years for all three datasets)
 crimeData <- filter(newCrime, (as.Date(newCrime$Date) >= as.Date("2013-1-15")) & (as.Date(newCrime$Date) <= as.Date("2014-12-30")))
 newCrime = NULL
@@ -42,9 +51,49 @@ newCrime = NULL
 #trafficData dates are already in the range specified
 #NOTE: CHECK FORMATTING IS IDENTICAL TO THE OTHERS (ISO)
 
+
+#
+#FIX WEATHER DATASET
+#
+
 #Fix variable names for weather
+#NOTE: TODO
+
+#Remove superfluous weather variables
+#NOTE: TODO
 
 
 
+
+
+#
+#FIX TRAFFIC DATASET
+#
+
+#Remove traffic observations that have no data
+trafficData <- trafficData[!(trafficData$SPEED=="-1"),]
+
+#Remove superfluous traffic variables
+#NOTE: TODO
+
+
+
+
+
+#
+#FIX CRIME DATASET
+#
+
+#Remove crime observations that have no applicable data (missing location, or type of crime)
+#NOTE: TODO
+
+#Remove superfluous crime variables
+#NOTE: TODO
+
+
+
+#
+# COMBINE DATASETS
+#
 
 
